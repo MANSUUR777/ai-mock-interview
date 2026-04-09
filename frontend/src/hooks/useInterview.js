6a88ea7ea67e76f75e949801aa302200b9a8e347
Mansuur Mohammad <87631376+MANSUUR777@users.noreply.github.com>
2026-04-09 09:26:23 +0530

    useInterview.js

--- frontend/src/hooks/useInterview.js
+++ frontend/src/hooks/useInterview.js
@@ -1,7 +1,8 @@
 import { useState } from 'react';
 import axios from 'axios';
 
-const API_BASE = import.meta.env.VITE_API_URL || 'http://127.0.0.1:5000';
+const API_BASE = "https://ai-mock-interview-rha3.onrender.com";
+
 
 export const useInterview = () => {
   const [role, setRole] = useState('');
